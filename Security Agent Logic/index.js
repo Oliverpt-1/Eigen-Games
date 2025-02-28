85f4eef6adfad894dcfc2813975734c932b019c0
0xjackson <jacksonsichelstiel@gmail.com>
2025-02-27 23:34:57 -0700

    Cleanup

--- Security Agent Logic/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const app = require("./configs/app.config")
-const PORT = process.env.port || process.env.PORT || 4004
-const dalService = require("./src/dal.service");
-
-dalService.init();
-app.listen(PORT, () => console.log("Server started on port:", PORT))
-
